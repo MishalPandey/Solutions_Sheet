6921e2a3259bb1bbe1b1e24563a7ca9b4a988337
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-10 15:55:52 +0530

    Update 5_Sort_an_array_of_0s_1s_2s.cpp

--- Strivers_Sde_Sheet/5_Sort_an_array_of_0s_1s_2s.cpp
+++ Strivers_Sde_Sheet/5_Sort_an_array_of_0s_1s_2s.cpp
@@ -1,1 +1,40 @@
+// Solution 1
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
 
+          int n= nums.size();
+          int count_0=0, count_1=0, count_2=0;
+
+          for(int i=0; i<n; i++)
+          {
+                if(nums[i]==0){
+                    count_0++;
+                }
+                else if(nums[i]==1){
+                    count_1++;
+                }
+                else{
+                    count_2++;
+                }
+          }
+          for(int i=0; i<n; i++)
+          {
+                if(count_0>0){
+                    nums[i]=0;
+                    count_0--;
+                }
+                else if(count_1>0){
+                    nums[i]=1;
+                    count_1--;
+                }
+                else{
+                    nums[i]=2;
+
+                }
+          }
+
+
+        
+    }
+};
