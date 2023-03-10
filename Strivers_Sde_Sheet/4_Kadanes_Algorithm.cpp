a112c489ff1f80e80c7aee32a1e5d2a2851ac185
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-10 12:20:32 +0530

    Update 4_Kadanes_Algorithm.cpp

--- Strivers_Sde_Sheet/4_Kadanes_Algorithm.cpp
+++ Strivers_Sde_Sheet/4_Kadanes_Algorithm.cpp
@@ -1,1 +1,43 @@
+// Solution1 
+// Kadane’s Algorithm of Maximum sum of Subarray
+// Kadane’s Algorithm is not valid for when all elements of array is negative
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
 
+        int n = nums.size();
+
+        int flag =1, ans=INT_MIN;
+        for(int i=0; i<n; i++)
+        { 
+            if(nums[i]>0){
+                flag=0;
+            }
+            ans= max(ans, nums[i]);
+
+        }
+        if(flag)
+        {
+            return ans;
+        }
+        
+
+        // Kadane’s Algorithm
+        int maxsum=0, currsum=0;
+        for(int i=0; i<n; i++)
+        {
+            currsum= currsum + nums[i];
+            if(currsum> maxsum)
+            {
+                maxsum= currsum;
+            }
+            if(currsum<0){
+                currsum=0;
+                
+            }
+
+        }
+        return maxsum;
+        
+    }
+};
