2fee4b3e8e0ebcb817784734291ad170e65446ba
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-11 00:18:26 +0530

    Update 5_Sort_an_array_of_0s_1s_2s.cpp

--- Strivers_Sde_Sheet/5_Sort_an_array_of_0s_1s_2s.cpp
+++ Strivers_Sde_Sheet/5_Sort_an_array_of_0s_1s_2s.cpp
@@ -1,4 +1,4 @@
-// Solution 1
+// Solution 1 --T.C(O(N+ N)) , S.C(O(1))
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
@@ -36,5 +36,38 @@ public:
 
 
         
+    }
+};
+
+
+//Solution 2 -- DUTCH NATIONAL FLAG ALGORITHM { T.C(O(N)) , S.C(O(1))  }
+// Optimum Solution
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0; // pointing to (1st One), always so that we can swap it with zero when mid is pointing to zero. --->initially pointing 0th index
+        int hi = nums.size() - 1;  // pointing to before index from (1st Two element)  always so that we can swap it with two when mid is pointing to two --->initially pointing last index
+        int mid = 0; // It will move next index when it is pointing the value which has value equal to One.
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
+            }
+        }
+         
     }
 };
