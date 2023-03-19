33e2599dbfbf37d53d535553ca875bd8009527a9
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-19 19:09:27 +0530

    Update 10_Find the_Duplicate_Number.cpp

--- Strivers_Sde_Sheet/10_Find the_Duplicate_Number.cpp
+++ Strivers_Sde_Sheet/10_Find the_Duplicate_Number.cpp
@@ -1,1 +1,44 @@
+// Solution 1----> T.C{O(nlogn)} S.C{O(1)}
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n-1; i++)
+        {
+            
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+            
+        }
+        return 0;
+    }
+};
+
+// Solution 2 Optimum   ----->  T.C{O(n)} S.C{O(1)} 
+// Linked List cycle method
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+
+        fast = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        
+        return slow;
+    }
+};
 
