9c29e8cccdc834e9f30a778a2a419cfc306cb51c
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-19 15:26:05 +0530

    Update 7_Rotate_Image.cpp

--- Strivers_Sde_Sheet/7_Rotate_Image.cpp
+++ Strivers_Sde_Sheet/7_Rotate_Image.cpp
@@ -1,1 +1,44 @@
+// Solution 1  ---> T.C{O(n*n)} S.C{O(n*n)}
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
 
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>>ans(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][j] = matrix[n- j- 1][i];
+            }
+        }
+        matrix = ans;
+
+        
+    }
+};
+
+//Solution 2 Optimum
+// T.C{O(n*n)} S.C{O(1)}
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        //transposing the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        //reversing each row of the matrix
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+
+        
+    }
+};
