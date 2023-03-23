6e5bacb09009b2d492e9213f04c8bb23275a8d42
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-24 00:43:12 +0530

    Create LinkedList_4_Remove_Nth_Node_From_End_of_List.cpp

---
+++ Strivers_Sde_Sheet/LinkedList_4_Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,44 @@
+// Solution 1---> T.C{O(n+n)}, S.C{O(1)}
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        int size=0;
+        ListNode* temp = head;
+        while(temp!= nullptr)
+        {
+            size++;
+            temp= temp->next;
+        }
+        int nth_removed_from_end= size- n;
+        temp = head;
+
+        if(nth_removed_from_end== 0)
+        {
+            head = head->next;
+            delete temp;
+            return head;
+        }
+    
+        while(--nth_removed_from_end)
+        {
+            temp= temp->next;
+        }
+        ListNode* Find= temp->next;
+        temp->next= temp->next->next;
+        delete Find;
+
+        return head;
+        
+    }
+};
