fc0d2cb8847b1c54dfef1aa502f7e0e795797b50
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-03-28 04:14:02 +0530

    Update LinkedList_6_Delete_Node_in_a_Linked_List.cpp

--- Strivers_Sde_Sheet/LinkedList_6_Delete_Node_in_a_Linked_List.cpp
+++ Strivers_Sde_Sheet/LinkedList_6_Delete_Node_in_a_Linked_List.cpp
@@ -1,1 +1,19 @@
+// Solution 1----> T.C{O(1)} , S.C{O(1)}
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+        node->val = node->next->val;
+        node->next = node->next->next;
+
+    }
+};
