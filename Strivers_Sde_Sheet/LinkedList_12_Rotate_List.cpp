1732ff07fb29afcbc55848f9d28b27d4c7024b1e
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-02 02:36:28 +0530

    Create LinkedList_12_Rotate_List.cpp

---
+++ Strivers_Sde_Sheet/LinkedList_12_Rotate_List.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+
+        if(head== nullptr || head->next == nullptr) return head;
+        int n=0;
+      
+            ListNode* temp= head, * end= nullptr;
+            while(temp != nullptr)
+            {
+                n++;
+                end = temp;
+                temp = temp->next;
+            }
+
+            k = k % n;
+            if(k == 0) return head;
+            k = n - k;
+            temp = head;
+            ListNode* last_end= temp;
+                while(k--)
+                {
+                    last_end = temp;
+                    temp = temp->next;
+                }
+                end->next = head;
+                last_end->next = nullptr;
+
+
+
+        return temp;
+    }
+};
+Console
