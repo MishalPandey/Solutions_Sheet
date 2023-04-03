4644a68af42aca83094fc34d9e5f009d2f631967
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-03 18:12:21 +0530

    Update LinkedListAndArrays_2_Clone_a_Linked_List_with_random_and_next_pointer.cpp

--- Strivers_Sde_Sheet/LinkedListAndArrays_2_Clone_a_Linked_List_with_random_and_next_pointer.cpp
+++ Strivers_Sde_Sheet/LinkedListAndArrays_2_Clone_a_Linked_List_with_random_and_next_pointer.cpp
@@ -1,1 +1,66 @@
+//Solution 1
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+
+        Node* newLL = new Node(-1), *ntemp = newLL, *temp = head;
+        map<Node*, int>add_idx;
+        //prev LL address with index stored
+        int i=1;
+        int size =0;
+        while(temp != nullptr)
+        {
+            size++;
+            add_idx[temp] = i;          
+            temp= temp->next;
+            i++;
+        }
+        temp = head;
+        map<int, Node*>nadd_idx;  //new LL index  with addres stored
+        i=1;
+        while(temp != nullptr)
+        {
+            ntemp->next = new Node(temp->val);
+            nadd_idx[i] = ntemp->next;
+            ntemp = ntemp->next;
+            temp= temp->next;
+            i++;
+            
+        }
+        temp = head;
+        ntemp = newLL->next;
+        while(temp != nullptr)
+        {
+            ntemp->random = nadd_idx[add_idx[temp->random]];
+            temp= temp->next;
+            ntemp= ntemp->next;
+            i++;
+            
+        }
+
+
+
+
+        return newLL->next;
+
+
+        
+    }
+};
