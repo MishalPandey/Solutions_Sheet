ce6e2f4cf97aea0543d92a125df8ae06c4659601
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-08 19:10:37 +0530

    Update BinaryTree_5_Morris_Preorder_Traversal.cpp

--- Strivers_Sde_Sheet/BinaryTree_5_Morris_Preorder_Traversal.cpp
+++ Strivers_Sde_Sheet/BinaryTree_5_Morris_Preorder_Traversal.cpp
@@ -1,1 +1,72 @@
+//Solution 1 --> Concept of travesal with S.C{O(1)}, T.C{O(3*n)}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
+
+
+class Solution {
+public:
+
+        TreeNode* get_rightmost_node(TreeNode* node, TreeNode* curr)
+        {
+                while(node->right != nullptr && node->right != curr)
+                {
+                    node= node->right;
+                }
+
+                return node;
+
+        }
+
+    
+        vector<int> preorderTraversal(TreeNode* root) {
+
+                vector<int>ans;
+                TreeNode *curr = root;
+
+                while(curr != nullptr)
+                {
+                        TreeNode *ln = curr->left; 
+
+                        if(ln != nullptr)
+                        {
+                                ln = get_rightmost_node(ln, curr); //righmost node of left child
+                                if(ln->right == nullptr)
+                                {
+                                    //create thread
+                                    ans.push_back(curr->val);
+                                    ln->right = curr;
+                                    curr = curr->left;
+
+                                }
+                                else // means ln== curr
+                                {
+                                    //cut thread
+                                    curr= curr->right;
+                                    ln->right = nullptr;
+
+                                }
+
+                        }
+                        else
+                        {
+                            ans.push_back(curr->val);
+                            curr = curr->right;
+
+                        }
+                }
+
+                
+                return ans;
+                
+        }
+};
