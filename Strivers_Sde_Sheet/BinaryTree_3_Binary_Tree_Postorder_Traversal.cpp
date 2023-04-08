32c4732fbd2ab149fdd38ef720f3f02933a7adfb
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-08 19:48:57 +0530

    Update BinaryTree_3_Binary_Tree_Postorder_Traversal.cpp

--- Strivers_Sde_Sheet/BinaryTree_3_Binary_Tree_Postorder_Traversal.cpp
+++ Strivers_Sde_Sheet/BinaryTree_3_Binary_Tree_Postorder_Traversal.cpp
@@ -140,3 +140,64 @@ public:
         
     }
 };
+
+
+//Solution 4 --> most optimal----(Morris Postorder Traversal) S.C{O(1)}, T.C{O(3*n)}
+class Solution {
+public:
+
+    TreeNode* get_rightmost_node(TreeNode* node, TreeNode* curr)
+    {
+            while(node->left != nullptr && node->left != curr)
+            {
+                node= node->left;
+            }
+
+            return node;
+
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+
+        vector<int>ans;
+        TreeNode *curr = root;
+
+        while(curr != nullptr)
+        {
+                TreeNode *ln = curr->right; 
+
+                if(ln != nullptr)
+                {
+                        ln = get_rightmost_node(ln, curr); //righmost node of right child
+                        if(ln->left == nullptr)
+                        {
+                            //create thread
+                            ans.push_back(curr->val);
+                            ln->left = curr;
+                            curr = curr->right;
+
+                        }
+                        else // means ln== curr
+                        {
+                            //cut thread
+                            curr= curr->left;
+                            ln->left = nullptr;
+
+                        }
+
+                }
+                else
+                {
+                    ans.push_back(curr->val);
+                    curr = curr->left;
+
+                }
+                
+        }
+        reverse(ans.begin(), ans.end());
+
+                
+        return ans;
+        
+    }
+};
