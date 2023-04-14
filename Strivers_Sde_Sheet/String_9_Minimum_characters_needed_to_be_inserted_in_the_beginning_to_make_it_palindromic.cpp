6a7c17a9d4868f268247b9e8a7c11707daf39454
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-14 19:35:16 +0530

    Update String_9_Minimum_characters_needed_to_be_inserted_in_the_beginning_to_make_it_palindromic.cpp

--- Strivers_Sde_Sheet/String_9_Minimum_characters_needed_to_be_inserted_in_the_beginning_to_make_it_palindromic.cpp
+++ Strivers_Sde_Sheet/String_9_Minimum_characters_needed_to_be_inserted_in_the_beginning_to_make_it_palindromic.cpp
@@ -1,5 +1,7 @@
 //Solution 1---T.C{O(n^2)}, S.C{O(1)}
 
+// code when Revise String
+
 int palin_max(string s)
 {
         int n = s.size();
@@ -61,5 +63,87 @@ int minCharsforPalindrome(string str) {
    
 	
 	
+}
+
+
+
+
+
+//Solution 2 -----T.C{O(n)},  S.C{O(n)} --------------USING KMP algorithm, String_8_KMP algo / LPS(pi) array question
+
+
+
+
+// C++ program for getting minimum character to be
+// added at front to make string palindrome
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// returns vector lps for given string str
+vector<int> computeLPSArray(string str)
+{
+    int M = str.length();
+    vector<int> lps(M);
+ 
+    int len = 0;
+    lps[0] = 0; // lps[0] is always 0
+ 
+    // the loop calculates lps[i] for i = 1 to M-1
+    int i = 1;
+    while (i < M)
+    {
+        if (str[i] == str[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (str[i] != str[len])
+        {
+            // This is tricky. Consider the example.
+            // AAACAAAA and i = 7. The idea is similar
+            // to search step.
+            if (len != 0)
+            {
+                len = lps[len-1];
+ 
+                // Also, note that we do not increment
+                // i here
+            }
+            else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+ 
+// Method returns minimum character to be added at
+// front to make string palindrome
+int getMinCharToAddedToMakeStringPalin(string str)
+{
+    string revStr = str;
+    reverse(revStr.begin(), revStr.end());
+ 
+    // Get concatenation of string, special character
+    // and reverse string
+    string concat = str + "$" + revStr;
+ 
+    //  Get LPS array of this concatenated string
+    vector<int> lps = computeLPSArray(concat);
+ 
+    // By subtracting last entry of lps vector from
+    // string length, we will get our result
+    return (str.length() - lps.back());
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    string str = "AACECAAAA";
+    cout << getMinCharToAddedToMakeStringPalin(str);
+    return 0;
 }
 
