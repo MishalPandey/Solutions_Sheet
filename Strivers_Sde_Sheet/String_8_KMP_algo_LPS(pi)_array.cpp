90133c9eeaff7fd9dff4e52a0124308d0fc2c458
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-14 16:48:20 +0530

    Update String_8_KMP_algo_LPS(pi)_array.cpp

--- Strivers_Sde_Sheet/String_8_KMP_algo_LPS(pi)_array.cpp
+++ Strivers_Sde_Sheet/String_8_KMP_algo_LPS(pi)_array.cpp
@@ -1,2 +1,6 @@
 //Knuth-Morris-Pratt KMP String Matching Algorithm
 //for problems like --- String_7_Find_the_Index_of_the_First_Occurrence_in_a_String.cpp
+
+//link --- algorithm---(https://youtu.be/V5-7GzOfADQ)
+
+//Solution 1 --T.C{O(n+m)}, S.C{O(n)}
