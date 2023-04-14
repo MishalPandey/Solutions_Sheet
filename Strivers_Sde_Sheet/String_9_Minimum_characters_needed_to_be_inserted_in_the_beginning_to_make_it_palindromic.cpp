4f65481a6692a67df41bb0329445c8bd139804b4
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-14 19:18:54 +0530

    Update String_9_Minimum_characters_needed_to_be_inserted_in_the_beginning_to_make_it_palindromic.cpp

--- Strivers_Sde_Sheet/String_9_Minimum_characters_needed_to_be_inserted_in_the_beginning_to_make_it_palindromic.cpp
+++ Strivers_Sde_Sheet/String_9_Minimum_characters_needed_to_be_inserted_in_the_beginning_to_make_it_palindromic.cpp
@@ -1,1 +1,65 @@
-//Solution 1
+//Solution 1---T.C{O(n^2)}, S.C{O(1)}
+
+int palin_max(string s)
+{
+        int n = s.size();
+        int i=0, j= n-1;
+        int ans=0;
+        int flag =0;
+        int prev=n-1;
+        while(i<j)
+        {
+              if(s[i]!=s[j] )
+              {
+                    if(flag==1)
+                    {
+                          j = prev-1;
+                    }
+                    else
+                    {
+                          j--;
+                    }
+                    ans++;
+                    i=0;
+                    flag=0;
+
+              }
+              else{
+                    if(flag==0)
+                    {
+                          prev = j;
+
+                    }
+                    i++;
+                    j--;
+                    flag=1;
+
+          }
+
+          if(i>=j && flag)
+          {
+                return ans;
+
+          }
+        }
+
+        return n -1;
+}
+
+
+int minCharsforPalindrome(string str) {
+	
+	int n= str.size();
+	if(n==1)
+		return 0;
+	
+	int ans= palin_max(str);
+	
+	
+	return ans;
+
+   
+	
+	
+}
+
