aaf19e93477ad60120184b10d6bf71b3773f7623
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-05-28 15:37:41 +0530

    Create Graph_DFS.cpp

---
+++ Strivers_Sde_Sheet/Graph_DFS.cpp
@@ -0,0 +1,32 @@
+//Solution 1, T.C{O(V+E)}, S.C{O(V)}
+void dfs(int V, vector<int> adj[],  vector<int> & ans, int x, vector<bool>&vis)
+    {
+        
+        ans.push_back(x);
+        vis[x]= true;
+        for(auto it: adj[x])
+        {
+            if(vis[it]) continue;
+            dfs(V, adj[], ans, it, vis);
+            
+        }
+        
+        
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>ans;
+        vector<bool> vis(V, 0);
+        
+        if(V>0)
+        {
+            dfs(V, adj[], ans, 0, vis)
+        
+        }
+        
+        
+        return ans;
+        
+        
+    }
