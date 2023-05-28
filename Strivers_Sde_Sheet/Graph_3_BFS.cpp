5e896493795da0c27da5b2769f976c83411e303f
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-05-28 15:56:34 +0530

    Create Graph_3_BFS.cpp

---
+++ Strivers_Sde_Sheet/Graph_3_BFS.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    void bfs(int V, vector<int> adj[],  vector<int> &ans, vector<bool>&vis)
+    {
+        
+        queue<int>q;
+        q.push(0);
+        while(q.size())
+        {
+            int top = q.front();
+            q.pop();
+            if(vis[top]) continue;
+            vis[top]= true;
+            ans.push_back(top);
+            for(auto it: adj[top])
+            {
+                q.push(it);
+            }
+            
+        }
+        
+        
+    }
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>ans;
+        vector<bool> vis(V, 0);
+        
+        bfs(V, adj, ans, vis);
+        
+        
+        return ans;
+    }
+};
