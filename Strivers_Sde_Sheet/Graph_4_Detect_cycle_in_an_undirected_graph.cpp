95bd68dd19ca42855ac9e52f7da9bcd1de313a9f
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-05-29 14:32:52 +0530

    Create Graph_4_Detect_cycle_in_an_undirected_graph.cpp

---
+++ Strivers_Sde_Sheet/Graph_4_Detect_cycle_in_an_undirected_graph.cpp
@@ -0,0 +1,32 @@
+//Using DFS
+void dfs(int V, vector<int> adj[],  vector<int> & ans, int x, vector<bool>&vis)
+    {
+        
+        ans.push_back(x);
+        vis[x]= true;
+        for(auto it: adj[x])
+        {
+            if(vis[it]) continue;
+            dfs(V, adj, ans, it, vis);
+            
+        }
+        
+        
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>ans;
+        vector<bool> vis(V, 0);
+        
+        if(V>0)
+        {
+            dfs(V, adj, ans, 0, vis);
+        
+        }
+        
+        
+        return ans;
+        
+        
+    }
