2ba7ed8f3a4a50d02544322c76b3b41a03198347
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-05-29 14:48:02 +0530

    Update Graph_4_Detect_cycle_in_an_undirected_graph.cpp

--- Strivers_Sde_Sheet/Graph_4_Detect_cycle_in_an_undirected_graph.cpp
+++ Strivers_Sde_Sheet/Graph_4_Detect_cycle_in_an_undirected_graph.cpp
@@ -1,4 +1,4 @@
-//Solution 1, T.C{O(V+E)}, S.C{O(V)}
+//Solution 1, T.C{O(V+E)}, S.C{O(V)} --LUV SIR
 //Using DFS
 class Solution {
   public:
@@ -33,6 +33,61 @@ class Solution {
             {
                 int is_cycle = 0;
                 dfs(V, adj, is_cycle, vis, i, -1);
+                ans = ans | is_cycle;
+            }
+        }
+        
+        
+        return ans;
+    }
+};
+
+
+//Solution 2, T.C{O(V+E)}, S.C{O(V)} -- SUMIT SIR
+//Using BFS
+
+class Solution {
+  public:
+    void bfs(int V, vector<int> adj[],  int &is_cycle, vector<bool>&vis, int vertex)
+    {
+        
+        queue<int>q;
+        q.push(vertex);
+        while(q.size())
+        {
+            int top = q.front();
+            q.pop();
+            
+            if(vis[top])
+            {
+                is_cycle = 1;
+                continue;
+            }
+            vis[top]= true;
+            
+            for(auto it: adj[top])
+            {
+                if(vis[it]) continue; 
+                q.push(it);
+            }
+            
+        }
+        
+        
+    }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        
+        vector<bool> vis(V, 0);
+        bool ans = false;
+        
+        for(int i=0; i< V; i++)
+        {
+            
+            if(vis[i]== false)
+            {
+                int is_cycle = 0;
+                bfs(V, adj, is_cycle, vis, i);
                 ans = ans | is_cycle;
             }
         }
