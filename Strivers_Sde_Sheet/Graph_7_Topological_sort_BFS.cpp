4ac5d72cdb49835979335526fd2eb58ecb3f822d
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-06-02 15:49:18 +0530

    Create Graph_7_Topological_sort_BFS.cpp

---
+++ Strivers_Sde_Sheet/Graph_7_Topological_sort_BFS.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	//Kahns algorithm
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	   vector<int> ans;
+	   
+	   vector<int> indegree(V, 0);
+	   
+	   for(int i=0; i<V; i++)
+	   {
+	       for(auto it: adj[i])
+	       {
+	           indegree[it]++;
+	       }
+	   }
+	   
+	   queue<int>q;
+	   for(int i=0; i<V; i++)
+	   {
+	       if(indegree[i]==0)
+	       {
+	           q.push(i);
+	       }
+	   }
+	   
+	   while(q.size())
+	   {
+	       int ver = q.front();
+	       q.pop();
+	       ans.push_back(ver);
+	       
+	       for(auto it: adj[ver])
+	       {
+	           indegree[it]--;
+	           
+	           if(indegree[it]==0)
+	           {
+	               q.push(it);
+	           }
+	       }
+	       
+	   }
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   return ans;
+	}
+};
+
