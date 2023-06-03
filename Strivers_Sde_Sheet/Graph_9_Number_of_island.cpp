264281c3cc7e0fa6b82cdccb608443808972b8a7
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-06-03 13:57:48 +0530

    Create Graph_9_Number_of_island.cpp

---
+++ Strivers_Sde_Sheet/Graph_9_Number_of_island.cpp
@@ -0,0 +1,56 @@
+//Solution 1
+
+
+class Solution {
+public:
+
+    void combine_islands(vector<vector<char>>& grid, int n, int m,  int i, int j, vector<vector<bool>>&vis)
+    {
+        vis[i][j]= true;
+
+        if(i+1<n && !vis[i+1][j] && grid[i+1][j]=='1')
+        {
+            combine_islands(grid, n, m, i+1, j, vis);
+        }
+        if(i-1>=0 && !vis[i-1][j] && grid[i-1][j]=='1')
+        {
+            combine_islands(grid, n, m, i-1, j, vis);
+        }
+        if(j+1<m && !vis[i][j+1] && grid[i][j+1]=='1')
+        {
+            combine_islands(grid, n, m, i, j+1, vis);
+        }
+        if(j-1>=0 && !vis[i][j-1] && grid[i][j-1]=='1')
+        {
+            combine_islands(grid, n, m, i, j-1, vis);
+        }
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<bool>>vis(n, vector<bool>(m, 0));
+
+        int ans=0;
+
+        for(int i=0; i< n; i++)
+        {
+            for(int j=0; j< m; j++)
+            {
+                if(!vis[i][j] && grid[i][j]=='1')
+                {
+                    ans++;
+                    combine_islands(grid, n, m, i, j, vis);
+
+                }
+
+
+            }
+        }
+
+        return ans;
+        
+    }
+};
